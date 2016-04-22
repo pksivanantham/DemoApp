d1ffcc856d3de44b9189a296797acd835eea3ddd
Sivanantham Padikkasu <pksivanantham@gmail.com>
2016-04-23 00:20:54 +0530

    BLL and DAL Default files added

---
+++ DemoApp.BLL/DemoAppService.cs
@@ -0,0 +1,14 @@
+﻿using DemoApp.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp.BLL
+{
+    public class DemoAppService:IDemoAppService
+    {
+         
+    }
+}
